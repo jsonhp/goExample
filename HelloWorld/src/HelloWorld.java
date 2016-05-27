f0ecc61644055213d4269540eec56299746fca2f
Jeisson Herrera Peña <jeisson.herrera@techandsolve.com>
2016-05-27 10:52:24 -0500

    Example

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jeisson_herrera on 27/05/16.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
